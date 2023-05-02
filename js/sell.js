a3ad81f6d23da4327acf7ca3abadadd4f2c05b29
Proy <61022544+AsheFrayne@users.noreply.github.com>
2023-05-02 16:43:51 +0800

    Sell unfinished

---
+++ js/sell.js
@@ -0,0 +1,18 @@
+$(".nBtn1").click(function(){
+    $(".fg1").hide();
+    $(".fg2").show();
+
+});
+
+$(".nBtn2").click(function(){
+    $(".fg2").hide();
+    $(".fg3").show();
+});
+$(".pBtn2").click(function(){
+    $(".fg2").show();
+    $(".fg3").hide();
+});
+$(".pBtn1").click(function(){
+    $(".fg1").show();
+    $(".fg2").hide();
+});
